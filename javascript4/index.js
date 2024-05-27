3c7c1246056bd013638f26e585c9885e96c08097
Gabriel Seidi Takeda e Silva <gabriel.takeda@itriad.org.br>
2024-05-27 17:11:51 -0400

    exericico js4

--- javascript4/index.js
+++ javascript4/index.js
@@ -1,17 +1,17 @@
-function handleBotao() {
+console.log('amor doce');
+const botao = document.getElementById('botao');
+const input = document.getElementById("input");
+const area = document.getElementById('area');
+const circunferencia = document.getElementById('circu');
 
-    const botao = document.getElementById('botao')
-    const input = document.getElementById("input")
-    const area = document.getElementById('area')
-    const circuferencia = document.getElementById('circuferencia')
-
-    botao.onclick = () => {
-        const resultado = calcularValores(parseFloat(input.innerHTML))
-        area.innerHTML = resultado[0]
-        circuferencia.innerHTML = resultado[1]
-    }
+botao.onclick = (event) => {
+    event.preventDefault();
+    console.log('amor doce 2');
+    const resultado = calcularValores(parseFloat(input.value));
+    area.placeholder = resultado[0];
+    circunferencia.placeholder = resultado[1];
 }
 
 const calcularValores = (raio) => {
-    return [ 3.14 * raio * raio, 2 * 3.14 * raio]
+    return [ 3.14 * raio * raio, 2 * 3.14 * raio];
 }
