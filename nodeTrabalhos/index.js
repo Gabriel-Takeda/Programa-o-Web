d1604c672e8577106bfce6c5c47ed7fdc3584ce8
Gabriel-Takeda <gabrielseiditsilva@gmail.com>
2024-07-04 00:42:50 -0300

    nova entrega

--- nodeTrabalhos/index.js
+++ nodeTrabalhos/index.js
@@ -8,7 +8,6 @@ const PORT = process.env.PORT ?? 8080;
 
 let valores = [];
 
-// Coleta dos argumentos da linha de comando
 process.argv.forEach((val, index) => {
     if (index > 1) {
         valores.push(val);
@@ -17,25 +16,23 @@ process.argv.forEach((val, index) => {
 
 let arquivos_nomes = [];
 
-// Leitura do diretório passado como argumento
 fs.readdir(valores[0], (err, arquivos) => {
     if (err) throw err;
     
-    arquivos_nomes = arquivos; // Mantém arquivos_nomes como um array
+    arquivos_nomes = arquivos; 
     arquivos.forEach((arquivo) => {
         console.log(arquivo);
     });
 
-    // Criação do servidor HTTP após a leitura dos arquivos
     const server = http.createServer((req, res) => {
         const { url } = req;
 
         if (url === '/') {
             res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-            res.write(createLink(arquivos_nomes)); // Passa o array para createLink
+            res.write(createLink(arquivos_nomes)); 
             res.end();
         } else {
-            const fileName = url.slice(1); // Remove a barra inicial
+            const fileName = url.slice(1); 
             const filePath = `${valores[0]}/${fileName}`;
 
             fs.readFile(filePath, 'utf-8', (err, data) => {
@@ -46,7 +43,7 @@ fs.readdir(valores[0], (err, arquivos) => {
                 } else {
                     res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
                     res.write(`<pre>${data}</pre>`);
-                    res.write('<br><a href="/">Voltar</a>'); // Link para voltar
+                    res.write('<br><a href="/">Voltar</a>'); 
                     res.end();
                 }
             });
